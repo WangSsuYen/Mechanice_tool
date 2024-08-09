680f8bf69a1b17781bfa06e4f032f37bc8326614
unknown <rockwang@yangcnc.com>
2024-08-09 16:50:58 +0800

    ScrewPanel GUI compelet

--- operation.py
+++ operation.py
@@ -1,31 +1,56 @@
 import wx
 
+# 螺桿推力計算
 class ScrewThrustPanel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
         self.InitUI()
 
     def InitUI(self):
+        # 畫欄位
         sizer = wx.BoxSizer(wx.VERTICAL)
         self.SetSizer(sizer)
+        # 添加圖片格子
+        image = wx.Image('images/Bearing.jpg', wx.BITMAP_TYPE_JPEG)
+        image = image.Scale(200, 200, wx.IMAGE_QUALITY_HIGH)
+        bitmap = wx.StaticBitmap(self, -1, wx.Bitmap(image))
+        sizer.Add(bitmap, 0, wx.ALIGN_CENTER | wx.ALL, 10)
 
-        self.AddLabeledTextCtrl(sizer, "馬達力矩：")
-        self.AddLabeledTextCtrl(sizer, "螺桿直徑：")
-        self.AddLabeledTextCtrl(sizer, "螺桿導程：")
-        self.AddLabeledTextCtrl(sizer, "導程角：")
-        self.AddLabeledTextCtrl(sizer, "損失估算：")
+        # 畫輸入欄位
+        form_sizer = wx.GridSizer(cols=2, vgap=30, hgap=30)
+        sizer.Add(form_sizer, 0, wx.ALIGN_CENTER | wx.ALL, 10)
+        # 新增欄位
+        self.AddLabeledTextCtrl(form_sizer, '馬達力矩：', "N.m")
+        self.AddLabeledTextCtrl(form_sizer, "螺桿直徑：", 'mm')
+        self.AddLabeledTextCtrl(form_sizer, "螺桿導程：", "mm")
+        self.AddLabeledTextCtrl(form_sizer, "導程角：", 'Deg')
+        self.AddLabeledTextCtrl(form_sizer, "損失估算：", "%")
 
-        calc_button = wx.Button(self, label="送出計算")
-        sizer.Add(calc_button, 0, wx.EXPAND | wx.ALL, 5)
+        # 設定按鈕圖案
+        btn_icon = wx.Image("images/submit.png", wx.BITMAP_TYPE_PNG)
+        btn_icon = btn_icon.Scale(80, 70, wx.IMAGE_QUALITY_HIGH)
+        btn_bitmap = wx.Bitmap(btn_icon)
+        # 添加計算按鈕
+        calc_button = wx.BitmapButton(self, bitmap=btn_bitmap)
+        calc_button.SetBackgroundColour(wx.Colour(255,0,0))
+        sizer.Add(calc_button, 0, wx.ALIGN_CENTER | wx.ALL, 10)
 
-    def AddLabeledTextCtrl(self, sizer, label):
+
+    def AddLabeledTextCtrl(self, sizer, label, unit):
         box = wx.BoxSizer(wx.HORIZONTAL)
-        lbl = wx.StaticText(self, label=label)
-        txt = wx.TextCtrl(self)
-        box.Add(lbl, 0, wx.ALL, 5)
-        box.Add(txt, 1, wx.ALL, 5)
+        # 抬頭
+        lbl = wx.StaticText(self, label=label, size=(80,20))
+        box.Add(lbl, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+        # 輸入框
+        txt = wx.TextCtrl(self, size=(100,20))
+        box.Add(txt, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+        # 單位
+        unit_lbl = wx.StaticText(self, label=unit)
+        box.Add(unit_lbl, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+        # 添加到主布局
         sizer.Add(box, 0, wx.EXPAND | wx.ALL, 5)
 
+# 皮帶計算
 class BeltPanel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
@@ -52,6 +77,7 @@ class BeltPanel(wx.Panel):
         box.Add(txt, 1, wx.ALL, 5)
         sizer.Add(box, 0, wx.EXPAND | wx.ALL, 5)
 
+# 流體力學計算
 class FluidMechanicsPanel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
--- reference_app.py
+++ reference_app.py
@@ -1,6 +1,8 @@
 import wx
 from operation import ScrewThrustPanel, BeltPanel, FluidMechanicsPanel
 
+
+# 側畫面
 class SideMenu(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
@@ -8,11 +10,18 @@ class SideMenu(wx.Panel):
         self.InitUI()
 
     def InitUI(self):
+        # 背景、視窗大小設定
         self.SetBackgroundColour(wx.Colour(0, 255, 255))
         self.SetMinSize((200, 0))
-
+        # 視窗管理器
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.SetSizer(self.sizer)
+        # 抬頭
+        self.title = wx.StaticText(self, label="力學計算機", size=(200,60))
+        self.title.SetForegroundColour(wx.Colour(255, 0, 0))
+        font = wx.Font(25, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
+        self.title.SetFont(font)
+        self.sizer.Add(self.title, 0, wx.ALIGN_CENTER | wx.ALL, 10)
 
         self.formulas = ['螺桿推力', '皮帶', '流體力學']
         for formula in self.formulas:
@@ -25,30 +34,38 @@ class SideMenu(wx.Panel):
         label = button.GetLabel()
         self.main_frame.SwitchPanel(label)  # 呼叫主框架的 SwitchPanel 方法
 
+
+# 主畫面
 class MainFrame(wx.Frame):
     def __init__(self, *args, **kw):
         super().__init__(*args, **kw)
         self.InitUI()
 
     def InitUI(self):
+        # 主畫面管理器
         self.panel = wx.Panel(self)
         self.sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.panel.SetSizer(self.sizer)
 
+        # 側邊欄位規劃
         self.side_menu = SideMenu(self.panel)
         self.sizer.Add(self.side_menu, 0, wx.EXPAND | wx.ALL, 5)
 
+        # 主畫面規劃
         self.main_sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.main_sizer, 1, wx.EXPAND | wx.ALL, 5)
 
+        # 選擇主頁類別
         self.screw_thrust_panel = ScrewThrustPanel(self.panel)
         self.belt_panel = BeltPanel(self.panel)
         self.fluid_mechanics_panel = FluidMechanicsPanel(self.panel)
 
+        # 類別匯入主頁
         self.main_sizer.Add(self.screw_thrust_panel, 1, wx.EXPAND | wx.ALL, 5)
         self.main_sizer.Add(self.belt_panel, 1, wx.EXPAND | wx.ALL, 5)
         self.main_sizer.Add(self.fluid_mechanics_panel, 1, wx.EXPAND | wx.ALL, 5)
 
+        # 其他類別隱藏
         self.screw_thrust_panel.Hide()
         self.belt_panel.Hide()
         self.fluid_mechanics_panel.Hide()
@@ -70,6 +87,8 @@ class MainFrame(wx.Frame):
             self.fluid_mechanics_panel.Show()
 
         self.panel.Layout()
+
+
 
 if __name__ == '__main__':
     app = wx.App(redirect=True, useBestVisual=True)
